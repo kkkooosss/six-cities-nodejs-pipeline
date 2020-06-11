e30374e0841ea24da50f206e4b36eeebf8b790fc
Alexander Kachar <alexkachar.github@gmail.com>
2020-06-11 22:51:51 +0300

    added App and Main components

---
+++ src/components/app/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import Main from "../main/main.jsx";
+
+// eslint-disable-next-line react/prop-types
+const App = ({offersCount}) => <Main offersCount={offersCount} />;
+
+export default App;
