2f70a8b82d4daded8c2ca520841611ba1355d92a
Alexander Kachar <alexkachar.github@gmail.com>
2020-06-21 22:19:22 +0300

    Added dev-router and remade OfferDetails

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Main from "../main/main.jsx";
+import OfferDetails from "../offer-details/offer-details.jsx";
 
-const App = ({offers, offersCount}) => (
-  <Main offers={offers} offersCount={offersCount} />
-);
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {offers, offersCount} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main offers={offers} offersCount={offersCount} />
+          </Route>
+          <Route exact path="/dev-details">
+            <OfferDetails offer={offers[0]} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offersCount: PropTypes.number.isRequired,
