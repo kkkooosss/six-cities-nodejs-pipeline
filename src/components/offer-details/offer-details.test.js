83c5d1f78fecc7e0547e71799ef2eacb5ede28f9
Alexander Kachar <alexkachar.github@gmail.com>
2020-06-22 22:58:03 +0300

    fixed OfferDetails snapshot test

--- src/components/offer-details/offer-details.test.js
+++ src/components/offer-details/offer-details.test.js
@@ -2,9 +2,18 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import OfferDetails from './offer-details.jsx';
 
+const OFFER = {
+  id: 1,
+  title: `Beautiful &amp; luxurious apartment at great location`,
+  type: `Apartment`,
+  isPremium: true,
+  price: 120,
+  img: `img/apartment-01.jpg`,
+};
+
 it(`OfferDetails renders correctly`, () => {
   const tree = renderer
-    .create(<OfferDetails
+    .create(<OfferDetails offer={OFFER}
     />)
     .toJSON();
   expect(tree).toMatchSnapshot();
