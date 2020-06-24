748d0c1093c6fd65f9f2b1c6ff9a5fa6a6513c9e
Alexander Kachar <alexkachar.github@gmail.com>
2020-06-24 22:45:50 +0300

    added ref in map and fixed tests

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -3,38 +3,46 @@ import leaflet from 'leaflet';
 import PropTypes from 'prop-types';
 import {OfferTypes} from '../../types/offers.js';
 
-const city = [52.38333, 4.9];
-const zoom = 12;
-const icon = leaflet.icon({
-  iconUrl: `img/pin.svg`,
-  iconSize: [30, 30]
-});
-
 class Map extends React.PureComponent {
   constructor(props) {
     super(props);
+
+    this.map = React.createRef();
   }
 
   componentDidMount() {
-    this.map = leaflet.map(`map`, {
+    const {offers} = this.props;
+
+    const city = [52.38333, 4.9];
+    const zoom = 12;
+    const icon = leaflet.icon({
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    });
+
+    const map = leaflet.map(this.map.current, {
       center: city,
       zoom,
       zoomControl: false,
       marker: true
     });
 
+    map.setView(city, zoom);
+
     leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`,
         {attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`})
-        .addTo(this.map);
+        .addTo(map);
 
-    const {offers} = this.props;
+    offers.map((offer) => leaflet.marker(offer.coordinates, {icon}).addTo(map));
+  }
 
-    offers.map((offer) => leaflet.marker(offer.coordinates, {icon}).addTo(this.map));
+  componentWillUnmount() {
+    this.map.current = null;
   }
 
   render() {
     return (
-      <section className="cities__map map" id="map"></section>
+      <section className="cities__map map" id="map" ref={this.map}></section>
     );
   }
 }
