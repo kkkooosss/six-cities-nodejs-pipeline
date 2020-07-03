5d82b860c09a1775f55d053824cf3800774b033b
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-03 21:36:35 +0300

    added OffersSort componet

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -4,10 +4,13 @@ import {connect} from 'react-redux';
 
 import OffersList from '../offers-list/offers-list.jsx';
 import OfferTypes from '../../types/offer.js';
+import OffersSort from '../offers-sort/offers-sort.jsx';
+import CitiesList from '../cities-list/cities-list.jsx';
 import Map from '../../components/map/map.jsx';
+
+
 import {getCitiesTitles} from '../../helpers/helpers.js';
 import {filterOffers, reduceCities} from '../../selectors/selectors.js';
-import CitiesList from '../cities-list/cities-list.jsx';
 import {ActionCreator} from '../../store/reducer.js';
 
 const Main = ({offers, selectedOffers, selectedCity, onCitySelect, onTitleClick}) => {
@@ -59,38 +62,7 @@ const Main = ({offers, selectedOffers, selectedCity, onCitySelect, onTitleClick}
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offersCount} places to stay in {selectedCity}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                  Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                  Top rated first
-                  </li>
-                </ul>
-                {/* <select className="places__sorting-type" id="places-sorting">
-                  <option className="places__option" value="popular" selected="">Popular</option>
-                  <option className="places__option" value="to-high">Price: low to high</option>
-                  <option className="places__option" value="to-low">Price: high to low</option>
-                  <option className="places__option" value="top-rated">Top rated first</option>
-                </select> */}
-              </form>
+              <OffersSort />
               <div className="cities__places-list places__list tabs__content">
                 <OffersList offers={offersToRender} onTitleClick={onTitleClick} isNearPlacesList={false} />
               </div>
