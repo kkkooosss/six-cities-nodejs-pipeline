f1a3f530181d33dcb113268df468c3133278f62b
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-04 12:35:06 +0300

    made offerList a functional component

--- src/components/offers-list/offers-list.jsx
+++ src/components/offers-list/offers-list.jsx
@@ -4,38 +4,17 @@ import PropTypes from 'prop-types';
 import OfferCard from '../offer-card/offer-card.jsx';
 import OfferTypes from '../../types/offer.js';
 
-class OffersList extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeCard: null
-    };
-
-    this.handleCardHover = this.handleCardHover.bind(this);
-  }
-
-  handleCardHover(offer) {
-    this.setState({
-      activeCard: offer,
-    });
-  }
-
-  render() {
-    const {offers, onTitleClick, isNearPlacesList} = this.props;
-
-    return (
-      <>
-        {offers.map((offer) => <OfferCard offer={offer} onCardHover={this.handleCardHover} onTitleClick={onTitleClick} key={offer.id} isNearPlacesCard={isNearPlacesList} />)}
-      </>
-    );
-  }
-}
+const OffersList = ({offers, onTitleClick, handleCardHover, isNearPlacesList}) => (
+  <>
+    {offers.map((offer) => <OfferCard offer={offer} onCardHover={handleCardHover} onTitleClick={onTitleClick} key={offer.id} isNearPlacesCard={isNearPlacesList} />)}
+  </>
+);
 
 export default OffersList;
 
 OffersList.propTypes = {
   offers: PropTypes.arrayOf(OfferTypes.isRequired).isRequired,
   onTitleClick: PropTypes.func,
+  handleCardHover: PropTypes.func,
   isNearPlacesList: PropTypes.bool.isRequired
 };
