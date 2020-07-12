764bc51ffc0af9d9bfa657e29d9080847958ca80
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-12 22:59:39 +0300

    edited api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const createAPI = () => {
+const Error = {
+  UNAUTHORIZED: 401,
+};
+
+const createAPI = (onUnathorized) => {
   const api = axios.create({
     baseURL: `https://4.react.pages.academy/six-cities`,
     timeout: 5000,
@@ -9,7 +13,17 @@ const createAPI = () => {
 
   const onSuccess = (response) => response;
 
-  const onFail = (error) => error;
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnathorized();
+
+      throw err;
+    }
+
+    throw err;
+  };
 
   api.interceptors.response.use(onSuccess, onFail);
 
