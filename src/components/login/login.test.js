eef76debb3e13f25a2cddfe6fd97412121f6b8c7
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-13 22:38:24 +0300

    added snapshot test in login

---
+++ src/components/login/login.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import Login from './login.jsx';
+
+const store = createStore(() => ({
+  USER: {
+    user: {
+      id: 1,
+      email: `mail@email.com`,
+      avatarUrl: `/static/avatar/8.jpg`,
+      isPro: false,
+      name: `Angelina`,
+    }
+  }
+}));
+
+it(`Header renders correctly`, () => {
+  const tree = renderer
+    .create(
+        <Provider store={store}>
+          <Login onSubmit={() => {}} />
+        </Provider>, {
+          createNodeMock: () => document.createElement(`div`)
+        })
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
