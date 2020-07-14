0b90e98c46b4edba592a682b701273f9455e8a74
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-15 00:19:04 +0300

    added validation in sign in form

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,25 +8,71 @@ class Login extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.emailRef = React.createRef();
-    this.passwordRef = React.createRef();
+    this.state = {
+      email: ``,
+      password: ``,
+      isValidEmail: false,
+      emailError: ``,
+      isValidPassword: false,
+      passwordError: ``
+    };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleEmailChange = this._handleEmailChange.bind(this);
+    this._handlePasswordChange = this._handlePasswordChange.bind(this);
   }
 
-  handleSubmit(evt) {
+  _handleEmailChange(evt) {
+    const email = evt.target.value;
+    const emailRegEx = RegExp(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+
+    const isValidEmail = emailRegEx.test(email);
+
+    const emailError = isValidEmail ? null : `Please enter a valid email adress`;
+
+    this.setState({
+      email,
+      isValidEmail,
+      emailError
+    });
+  }
+
+  _handlePasswordChange(evt) {
+    const password = evt.target.value;
+    const isValidPassword = password.length > 3;
+
+    const passwordError = isValidPassword ? null : `Sorry, the password is too short`;
+
+    this.setState({
+      password,
+      isValidPassword,
+      passwordError
+    });
+  }
+
+  _handleSubmit(evt) {
     const {onSubmit} = this.props;
+    const {isValidEmail, isValidPassword} = this.state;
+    const email = this.emailRef.current.value;
+    const password = this.passwordRef.current.value;
 
     evt.preventDefault();
 
-    onSubmit({
-      email: this.emailRef.current.value,
-      password: this.passwordRef.current.value
-    });
-
+    if (isValidEmail && isValidPassword) {
+      onSubmit({email, password});
+    }
   }
 
   render() {
+    const {isValidEmail, isValidPassword, emailError, passwordError} = this.state;
+    const isValid = isValidEmail && isValidPassword;
+    const ErrorMessageStyle = {
+      position: `absolute`,
+      top: `-20px`,
+      color: `#BF616A`,
+      paddingLeft: `15px`
+    };
+
     return (
 
       <div className="page page--gray page--login">
@@ -36,16 +82,32 @@ class Login extends React.PureComponent {
           <div className="page__login-container container">
             <section className="login">
               <h1 className="login__title">Sign in</h1>
-              <form className="login__form form" action="#" method="post" onSubmit={this.handleSubmit}>
-                <div className="login__input-wrapper form__input-wrapper">
+              <form className="login__form form" action="#" method="post" onSubmit={this._handleSubmit}>
+                <div className="login__input-wrapper form__input-wrapper" style={{position: `relative`}}>
+                  {emailError ? <span style={ErrorMessageStyle}>{emailError}</span> : null}
                   <label className="visually-hidden">E-mail</label>
-                  <input className="login__input form__input" type="email" name="email" placeholder="Email" required ref={this.emailRef} />
+                  <input
+                    className="login__input form__input"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                    ref={this.emailRef}
+                    onChange={this._handleEmailChange} />
                 </div>
-                <div className="login__input-wrapper form__input-wrapper">
+                <div className="login__input-wrapper form__input-wrapper" style={{position: `relative`}} >
+                  {passwordError ? <span style={ErrorMessageStyle}>{passwordError}</span> : null}
                   <label className="visually-hidden">Password</label>
-                  <input className="login__input form__input" type="password" name="password" placeholder="Password" required ref={this.passwordRef} />
+                  <input
+                    className="login__input form__input"
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                    ref={this.passwordRef}
+                    onChange={this._handlePasswordChange} />
                 </div>
-                <Link to="/"><button className="login__submit form__submit button" type="submit">Sign in</button></Link>
+                <Link to="/"><button className="login__submit form__submit button" type="submit" disabled={!isValid}>Sign in</button></Link>
               </form>
             </section>
             <section className="locations locations--login locations--current">
