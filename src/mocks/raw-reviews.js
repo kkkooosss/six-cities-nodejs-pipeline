4fbf8b1e9ac9b5963dc208466fe1f4dd3ed3f369
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-18 16:11:52 +0300

    added getReviews e2e test

---
+++ src/mocks/raw-reviews.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    "id": 1,
+    "user": {
+      "id": 17,
+      "is_pro": false,
+      "name": `Emely`,
+      "avatar_url": `https://htmlacademy-react-3.appspot.com/six-cities/static/avatar/8.jpg`
+    },
+    "rating": 4,
+    "comment": `This villa is perfect in every way: the view on mountains and waterfalls, the hot tub and the villa itself. The evening here became a great continuation of our journeys over country.`,
+    "date": `2020-07-01T08:50:12.875Z`
+  },
+  {
+    "id": 2,
+    "user": {
+      "id": 16,
+      "is_pro": true,
+      "name": `Mollie`,
+      "avatar_url": `https://htmlacademy-react-3.appspot.com/six-cities/static/avatar/7.jpg`
+    },
+    "rating": 5,
+    "comment": `Beautiful space, fantastic location and atmosphere, really a wonderful place to spend a few days. Will be back.`,
+    "date": `2020-06-28T08:50:12.875Z`
+  }
+];
