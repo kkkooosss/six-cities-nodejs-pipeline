a22f763ebf5cbe937900a5d4e66c1e53f85366aa
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-25 14:16:32 +0300

    history.back in with-login

--- src/hocs/with-login/with-login.jsx
+++ src/hocs/with-login/with-login.jsx
@@ -57,7 +57,7 @@ const withLogin = (Component) => {
       if (isValidEmail && isValidPassword) {
         onLogin({email, password});
       }
-      window.history.back();
+      history.back();
     }
 
     render() {
