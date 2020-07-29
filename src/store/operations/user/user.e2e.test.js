86e7cb5f25e63ae71977414ca6f7020e93251113
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-29 19:52:04 +0300

    added user operation e2e test

--- src/store/operations/user/user.e2e.test.js
+++ src/store/operations/user/user.e2e.test.js
@@ -1,20 +1,29 @@
 import MockAdapter from 'axios-mock-adapter';
+
 import createAPI from '../../../api/api.js';
 import Operation from './user.js';
 import {Actions} from '../../actions/user/user.js';
 import {AuthStatus} from '../../../helpers/constants.js';
+import rawUser from '../../../mocks/raw-user.js';
+import mockUser from '../../../mocks/user.js';
 
 const api = createAPI(() => {});
+const apiMock = new MockAdapter(api);
+
+const mockAuthData = {
+  email: `mail@email.com`,
+  password: `1234`
+};
+
+apiMock
+  .onGet(`/login`).reply(200, AuthStatus.noAuth)
+  .onPost(`/login`).reply(200, rawUser);
 
 describe(`User operation works correctly`, () => {
-  it(`Should make a correct API call to /login get authStatus`, function () {
-    const apiMock = new MockAdapter(api);
+
+  it(`Should make a correct API call to /login and get authStatus`, function () {
     const dispatch = jest.fn();
     const dataLoader = Operation.checkAuthStatus();
-
-    apiMock
-      .onGet(`/login`)
-      .reply(200, AuthStatus.noAuth);
 
     return dataLoader(dispatch, () => {}, api)
       .then(() => {
@@ -24,6 +33,25 @@ describe(`User operation works correctly`, () => {
           payload: AuthStatus.noAuth
         });
       });
+  });
+
+  it(`Shoul make a correct API call to /login and sign in`, () => {
+    const dispatch = jest.fn();
+    const dataLoader = Operation.login(mockAuthData);
+
+    return dataLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: Actions.setUser,
+          payload: mockUser
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: Actions.setAuthStatus,
+          payload: AuthStatus.auth
+        });
+      });
+
   });
 
 });
