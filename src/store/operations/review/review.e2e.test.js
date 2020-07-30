e8519c006f354c9500780e759a4696c47cdb030a
Alexander Kachar <alexkachar.github@gmail.com>
2020-07-30 14:19:31 +0300

    added submitReview operation e2e test

--- src/store/operations/review/review.e2e.test.js
+++ src/store/operations/review/review.e2e.test.js
@@ -9,18 +9,42 @@ const api = createAPI(() => {});
 
 describe(`Review operation works correctly`, () => {
   it(`Should make a correct API call to /comments with id = 1 and get reviews`, function () {
-    const id = 1;
+    const offerId = 1;
     const apiMock = new MockAdapter(api);
     const dispatch = jest.fn();
     const dataLoader = Operation.loadReviews(1);
 
     apiMock
-      .onGet(`/comments/${id}`)
+      .onGet(`/comments/${offerId}`)
       .reply(200, rawReviews);
 
     return dataLoader(dispatch, () => {}, api)
       .then(() => {
         expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: Actions.getReviews,
+          payload: formatReviews(rawReviews)
+        });
+      });
+  });
+
+  it(`Should make a correct API call to /comments with id = 1 and post a review`, function () {
+    const offerId = 1;
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+
+    const rating = 5;
+    const text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
+
+    const dataLoader = Operation.submitReview(1, {rating, text});
+
+    apiMock
+      .onGet(`/comments/${offerId}`).reply(200, rawReviews)
+      .onPost(`/comments/${offerId}`).reply(200, rawReviews);
+
+    return dataLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(3);
         expect(dispatch).toHaveBeenNthCalledWith(1, {
           type: Actions.getReviews,
           payload: formatReviews(rawReviews)
