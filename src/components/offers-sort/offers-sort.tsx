81d892eac1264ee0e8fcc5a580e3e64495675095
Alexander Kachar <alexkachar.github@gmail.com>
2020-08-08 10:43:55 +0300

    fixed commented code presense in offers-sort.tsx

--- src/components/offers-sort/offers-sort.tsx
+++ src/components/offers-sort/offers-sort.tsx
@@ -64,12 +64,6 @@ class OffersSort extends React.PureComponent<Props> {
           {this._getFilterOptions(selectedFilter, onFilterSelect)}
         </ul> : null}
 
-        {/* <select className="places__sorting-type" id="places-sorting">
-        <option className="places__option" value="popular" selected="">Popular</option>
-        <option className="places__option" value="to-high">Price: low to high</option>
-        <option className="places__option" value="to-low">Price: high to low</option>
-        <option className="places__option" value="top-rated">Top rated first</option>
-      </select> */}
       </form>
     );
   }
